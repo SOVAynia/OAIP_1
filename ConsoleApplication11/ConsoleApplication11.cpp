ec962c458145ae93b3b5d4679b981d456c62b400
SOVA <SOVA@SOVA>
2025-12-25 18:28:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,18 @@
+﻿﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// e^-x-cosx+sin2xy
+
+int main()
+{
+	double x, y;
+
+	cout << "Введите x:";
+	cin >> x;
+	cout << "Введите Y:";
+	cin >> y;
+	double otvet = exp(-x) - cos(x) + sin(2 * x * y); 
+	cout << "Ответ: " << otvet << endl;
+	return 0;
+}
