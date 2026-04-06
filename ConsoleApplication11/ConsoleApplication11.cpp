c05f3fe5ebae2374d0565b3d3c82c904fcddfecc
SOVAynia <allregey@gmail.com>
2026-04-06 12:03:38 +0300

    Update ConsoleApplication11.cpp

--- ConsoleApplication11/ConsoleApplication11.cpp
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -1,13 +1,11 @@
 ﻿﻿#include <iostream> // используемые библиотеки
 #include <cmath> // Использовал библиотеку cmath
 using namespace std;
-
 // e^-x-cosx+sin2xy
-
 int main()
 {
 	double x, y;  // Переменные x и y с плавующей запятой
-
+// пустая строка
 	cout << "Введите x:";	//Ввод чисела
 	cin >> x;				// Чтение числа
 	cout << "Введите Y:";	// Ввод чисела
